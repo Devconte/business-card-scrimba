0e49a6e6033bf005adfb9670ba8aca1d23d8be91
Devconte <romain.lcnte@gmail.com>
2024-04-30 17:01:37 +0200

    feat: list component

---
+++ src/components/List/List.jsx
@@ -0,0 +1,16 @@
+function List() {
+  return (
+    <div>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Is maintained by Facebook</li>
+        <li>Allows you to create reusable UI components</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Powers thousands of enterprise apps including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
