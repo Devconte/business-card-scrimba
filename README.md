851095d33ae464864e025aa9e6a3d89a1eac850e
Devconte <romain.lcnte@gmail.com>
2024-04-30 17:00:24 +0200

    base configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Scrimba
+
+Simple react basics site from Scrimba course
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import List from "./components/List/List";
+
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
+
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <List />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
